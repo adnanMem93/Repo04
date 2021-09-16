91ddfdf0e623e41a4f3fa8cd51c85d871b76ef8b
adnanMem93 <adnan.mem.93@gmail.com>
2021-09-16 13:32:39 +0200

    Created folder 10

---
+++ ex10/src/index.js
@@ -0,0 +1,16 @@
+function main(){
+// only change code below this line
+var myFirstName = 'Adnan';
+var myLastName = 'Memidzan';
+var myHomeTown = 'Konjic';
+// only change code above this line
+return {
+    myFirstName,
+    myLastName,
+    myHomeTown
+};
+
+}
+    
+console.log(main());  
+module.exports = main;
