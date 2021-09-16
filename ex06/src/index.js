c0126dc9966e471507e11b46a4dce1e0b95bfb68
adnanMem93 <adnan.mem.93@gmail.com>
2021-09-16 12:28:22 +0200

    Created folder 06

---
+++ ex06/src/index.js
@@ -0,0 +1,10 @@
+function main(numOne,numTwo){
+// only change code below this line
+var myQuotient = numOne / numTwo; 
+// only change code above this line
+return myQuotient;
+
+}
+    
+console.log(main(numOne,numTwo));  // Change this line
+module.exports = main;
