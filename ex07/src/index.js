7bf0c15c0cbf659941de91d52560161f7bc92448
adnanMem93 <adnan.mem.93@gmail.com>
2021-09-16 12:35:35 +0200

    Created folder 07

---
+++ ex07/src/index.js
@@ -0,0 +1,10 @@
+function main(numOne,numTwo){
+// only change code below this line
+var remainder = numOne % numTwo; 
+// only change code above this line
+return remainder;
+
+}
+    
+console.log(main(numOne,numTwo));  // Change this line
+module.exports = main;
